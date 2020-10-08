aeda1cc86a2b0f669d4deb4a9f05826e69cf38a8
wjstmdwls <38906387+wjstmdwls@users.noreply.github.com>
2020-10-08 17:26:07 +0900

    Add files via upload

---
+++ aa1-15-rpt05/sketch05_multi_signals/sketch05_multi_signals.ino
@@ -0,0 +1,26 @@
+/*
+  Multi Signals
+  Simulation of multiple random signals
+*/
+// signals
+
+
+// the setup routine runs once when you press reset:
+void setup() {
+  // initialize serial communication at 9600 bits per second:
+  Serial.begin(9600);
+}
+
+// the loop routine runs over and over again forever:
+void loop() {
+  // Multi signals
+
+  
+  Serial.print("AA15, Ambient lux: ");
+  Serial.print(lux);
+  Serial.print(" , Humidity: ");
+  Serial.print(humi);
+  Serial.print(" , Temperature: ");
+  Serial.println(temp);
+  delay(500);        // delay in between reads for stability
+}
